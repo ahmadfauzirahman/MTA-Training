af9bc6ca16653120b5ec8e952b35d1b4fd930bc6
ojikdev <ahmadfauzirahman99@gmail.com>
2017-10-20 09:00:00 +0700

    Day Two Class , Object , Method ~

---
+++ DayTwo-ClassObjecMethod/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DayTwo_ClassObjecMethod
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Rectangle a = new Rectangle(20,5);
+            Console.WriteLine("Area of rectangle : {0}",a.GetArea());
+            
+        }
+    }
+}
---
+++ DayTwo-ClassObjecMethod/Rectangle.cs
@@ -0,0 +1,19 @@
+﻿namespace DayTwo_ClassObjecMethod
+{
+    public class Rectangle
+    {
+        private double length;
+        private double width;
+
+        public Rectangle(double l, double w)
+        {
+            this.length = l;
+            this.width = w;
+        }
+
+        public double GetArea()
+        {
+            return this.width * this.length;
+        }
+    }
+}

dbe4308fa79684af0aabcd4741638044f48427a1
ojikdev <ahmadfauzirahman99@gmail.com>
2017-10-20 09:31:57 +0700

    Day Two Class , Object , Method ~

---
+++ DayTwo-Constructors/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DayTwo_Constructors
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ DayTwo-Constructors/Rectangle.cs
@@ -0,0 +1,24 @@
+﻿namespace DayTwo_Constructors
+{
+    public class Rectangle
+    {
+        private double length;
+        private double width;
+
+        public Rectangle()
+        {
+        }
+
+        public Rectangle(double w)
+        {
+        }
+
+        public Rectangle(double w, double l)
+        {
+            length = l;
+            width = w;
+        }
+        
+        //construtor adalah method yang pertama kali dipanggil
+    }
+}

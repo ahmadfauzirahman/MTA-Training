7ad25a533eab89142d9e54031a10cb7c5530aae5
ojikdev <ahmadfauzirahman99@gmail.com>
2017-10-20 09:01:03 +0700

    Day Two Class , Object , Method ~

---
+++ DayTwo-Oop/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DayTwo_Oop
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x = 9;
+            int y = 4;
+            int z = x++ - y-- + (x + y);
+            Console.Write(--z % 2);
+            
+            Console.WriteLine("======================");
+            Console.WriteLine("======================");
+        
+            int[] a = new int[1];
+            a[0] = 3;
+            int b = 4;
+            Squere(ref b);
+            Console.WriteLine(a[0] +b);
+            Console.WriteLine("======================");
+        }
+        static int Squere(ref int x)
+        {
+            x *=x;
+            return x;
+        }
+    }
+}
